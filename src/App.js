b1ab5ea7cf7fe819873dca472beb338a214e3d84
AleProsDev <alejandro.f.pros@gmail.com>
2022-12-17 11:53:37 -0300

    comenzando

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
 import Buscador from './componentes/Buscador';
 import Card from './componentes/Card';
-//import ErrorMensaje from './componentes/ErrorMensaje';
 import Loading from './componentes/Loading';
 import NavBar from './componentes/NavBar';
 import { getListadoNoticias } from './servicios/ListadoAPI';
 import { useState } from 'react';
-import { isVisible } from '@testing-library/user-event/dist/utils';
-import ErrorMensaje from './componentes/ErrorMensaje';
-import { Outlet } from "react-router-dom";
+import Paginador from './componentes/Paginador';
 
 function App() {
 
@@ -23,30 +20,14 @@ function App() {
     setIsLoading(false)
   }
  
-  if(isLoading) {
-    return (
-    <div>
-    <Loading/>
-    </div>
-    )
-  }
-
-  if(isVisible) {
-    return (
-      <div>
-        <ErrorMensaje/>
-        <Outlet />
-      </div>
-    )
-  } else {
-    isVisible(false)
-  }
 
   return (
     <div className="App">
       <NavBar/>
+      {isLoading && <Loading/>}
       <Buscador onBuscar={onBuscar}/>
-      <Card/>
+      {noticias && <Card/>}
+      {noticias && <Paginador/>}
     </div>
   );
 }
